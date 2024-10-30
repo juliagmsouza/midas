ebc19471594fabe65d0d3b53466a5ed6b33f3892
Júlia Maia <47127259+juliagmsouza@users.noreply.github.com>
2024-10-30 17:13:58 -0300

    Update app.mjs

--- midas-get-data-lambda-main/hello-world/app.mjs
+++ midas-get-data-lambda-main/hello-world/app.mjs
@@ -1,16 +1,24 @@
-import express from 'express';
 import ProductRepository from './src/infra/repositories/product.repository.mjs';
 import GetProductGateway from './src/adapters/get-product.gateway.mjs';
 import GetProductUsecase from './src/usecases/get-product.mjs';
 
+const { Pool } = pg;
+const pool = new Pool({
+    connectionString: 'postgresql://root:9gnvu5tvk4gkzUJBk5k3cj89d2dm9tJb@dpg-cq5dqqlds78s73d0fn6g-a.oregon-postgres.render.com/midaspostgresql',
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
-const app = express()
-app.use(express.json());
-const port = 3000
-
-app.post('/', async (req, res) => {
-    const {coord, name } = req.body;
-
+export const lambdaHandler = async (event) => {
+    let client;
+    let messageBody;
+        const record = event.Records[0];
+        if (record) {
+            messageBody = JSON.parse(record.body).Message;
+        }
+        client = await pool.connect();
+ 
     // Repositories
     const productRepository = new ProductRepository({ client });
 
@@ -18,16 +26,15 @@ app.post('/', async (req, res) => {
     const logger = new Logger();
     
     // Gateways
-    const getProductGateway = new GetProductGateway({ productRepository, logger })
+    const getProductGateway = new GetProductGateway({ productRepository, logger });
 
     // Usecases
-    const getProductUsecase = new GetProductUsecase({ gateway: getProductGateway })
+    const getProductUsecase = new GetProductUsecase({ gateway: getProductGateway });
 
     // Execução
-    const productsList = await getProductUsecase.execute({ lat: coord[0], lng: coord[1], name });
+    const data = JSON.parse(messageBody);
+    const productsList = await getProductUsecase.execute({ lat: data.coord[0], lng: data.coord[1], data.name });
     res.json({ list: productsList });
-})
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    
+};
